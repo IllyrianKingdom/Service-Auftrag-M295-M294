403dfab9d2bdb64443152b6cdabf0635942dea43
John Doe <johndoe@example.com>
2026-01-08 14:15:09 +0100

    test

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import './navbar.css'
+
+function Navbar() {
+    return (
+
+            
+        <div className="navbar">
+           <header>AVA GmbH</header>
+           <header>Benutzer: </header>
+        </div>
+      
+    );
+}
+export default Navbar;
